fa4797b6d2c61b47f99fd6b0ac06bed52e6b0de0
Mason23Johnson <masonbjohnson@hotmail.com>
2025-03-03 15:21:43 -0600

    Time: 34 ms (80.27%) | Memory: 65.9 MB (44.82%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) 
     {
-
-        Array.Sort(nums); // sort array
+        Array.Sort(nums);
         List<IList<int>> result = new List<IList<int>>();
 
-        for (int i = 0; i < nums.Length - 2; i++)
+        for (int i = 0; i < nums.Length -2; i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1]) 
+            if (i > 0 && nums[i] == nums [i -1])
             {
-            continue;
+                continue;
             }
 
             int left = i + 1;
@@ -18,33 +17,36 @@ public class Solution {
             while (left < right)
             {
                 int sum = nums[i] + nums[left] + nums[right];
-
+            
                 if (sum == 0)
                 {
                     result.Add(new List<int> 
-                    { 
-                        nums[i], nums[left], nums[right] 
+                    {
+                        nums[i], nums[left], nums[right]
                     });
-
-                     while (left < right && nums[left] == nums[left + 1]) left++;
-                     while (left < right && nums[right] == nums[right - 1]) right--;
-
-                     left++;
-                     right--;
+                
+                    while (left < right && nums[left] == nums[left + 1])
+                    {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1])
+                    {
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
-
                 else if (sum < 0)
                 {
-                    left++; // Move left pointer right to inc sum
+                    left++; // moving left pointer to the right so we inc sum
                 }
                 else
                 {
-                    right--; // move right left to dec sum
+                    right--; // moving right pointer to the left so we dec sum
                 }
             }
         }
 
         return result;
-
     }
 }
